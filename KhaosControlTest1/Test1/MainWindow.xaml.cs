b1a9b2f1f9c29a004f3fcb37cbabd3afd8de7eda
JordanHassall <35333367+JordanHassall@users.noreply.github.com>
2020-08-22 17:57:19 +0100

    created solution and made initial ui

---
+++ KhaosControlTest1/Test1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Test1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> _itemList = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ItemList.ItemsSource = _itemList;
+        }
+
+        /// <summary>
+        /// Fills the itemlist if it hasn't already been filled, does nothing if it has.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FillListButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_itemList.Count == 0)
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    _itemList.Add($"This is item number {i}");
+                }
+            }
+        }
+    }
+}
